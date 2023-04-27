18d9c831abe2ef30b2229240082a526e05ddd97c
Alan Hornbaker <alanhornbaker@gmail.com>
2023-04-27 16:44:19 -0500

    Addressed TODO's in database.js, install.js, and webpack.config.js.  Need to test and debug.

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,47 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent Chrome 76 and later from automatically showing the prompt
+  event.preventDefault();
+
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+
+  // Update UI notify the user they can install the PWA
+  // Display an "Install" button or similar UI element to notify the user they can add the app to the home screen
+  // Show the buttonInstall button
+  butInstall.style.display = "block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+  if (deferredPrompt) {
+    // Show the install prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+
+    // Log the result of the prompt
+    console.log(`User response to the install prompt: ${outcome}`);
+
+    // Reset the deferredPrompt variable, since it can only be used once
+    deferredPrompt = null;
+
+    // Hide the buttonInstall button
+    butInstall.style.display = "none";
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  // Log the app installation
+  console.log("Jate PWA installed successfully.");
+});
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,29 +1,72 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      // Add the HtmlWebpackPlugin
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      // Add the WebpackPwaManifest plugin and configure it
+      new WebpackPwaManifest({
+        name: "My PWA",
+        short_name: "My PWA",
+        description: "My Progressive Web App",
+        start_url: ".",
+        background_color: "#ffffff",
+        theme_color: "#317EFB",
+        icons: [
+          {
+            src: path.resolve("src/assets/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            purpose: "maskable any",
+          },
+        ],
+      }),
+      // Add the GenerateSW plugin to generate the service worker
+      new GenerateSW(),
+      // Add the InjectManifest plugin to inject the manifest into the service worker
+      new InjectManifest({
+        swSrc: "./src/js/sw.js",
+      }),
     ],
 
     module: {
       rules: [
-        
+        // Adding css loaders and babel to webpack:
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
